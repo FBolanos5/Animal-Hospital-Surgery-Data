0190a6262eb050225db35398835bcd458791bc9b
Fbolanos5 <fbolanos@csu.fullerton.edu>
2024-12-02 03:26:00 -0800

    One day down, 364 to go

--- Surgery Data.R
+++ Surgery Data.R
@@ -1,1 +1,9 @@
-print("test")
+surgery_data <- data.frame(
+  species = c("Cat", "Dog", "Cat"),
+  surgery_type = c("Spay", "Spay", "Neuter"),
+  age = c(.5, 1,1),
+  surgery_date = as.Date(c("2024-01-02", "2024-01-02", "2024-01-02")),
+  stringsAsFactors = FALSE
+)
+print(surgery_data)
+
